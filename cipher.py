470475cbceb1aabfaba7c2644450b27e61a4db04
journey6 <blackflower24@gmail.com>
2019-05-15 21:48:06 +0300

    Add files via upload
    version 1.0

---
+++ cipher.py
@@ -0,0 +1,99 @@
+"""
+Author @journey6 (Hussain)
+Github link: https://github.com/journey6/
+this is implementation of Caesar cipher which shifts characters and letters by specific number positive or negative
+"""
+
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+small_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z']
+capital_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                   'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+def encoder(text, shift_value):
+    encoded_text = ""
+    for i, val in enumerate(text):
+        char_index = get_encoding_index(val, shift_value)
+        if str.islower(val):
+            encoded_text = encoded_text + small_letters[char_index]
+        elif str.isupper(val):
+            encoded_text = encoded_text + capital_letters[char_index]
+        elif str.isdigit(val):
+            encoded_text = encoded_text + numbers[char_index]
+        else:
+            encoded_text = encoded_text + val
+
+    return encoded_text
+
+
+def get_encoding_index(c, shift_value):
+    if str.islower(c):
+        for i, val in enumerate(small_letters):
+            if c == val:
+                shift_value = (i + shift_value % len(small_letters)) % len(small_letters)
+                return shift_value
+
+    elif str.isupper(c):
+        for i, val in enumerate(capital_letters):
+            if c == val:
+                shift_value = (i + shift_value % len(capital_letters)) % len(capital_letters)
+                return shift_value
+
+    elif str.isdigit(c):
+        for i, val in enumerate(numbers):
+            if c == val:
+                shift_value = (i + shift_value % len(numbers)) % len(numbers)
+                return shift_value
+
+    else:
+        return -1
+
+
+def decoder(text, shift_value):
+    decoded_text = ""
+    for i, val in enumerate(text):
+        char_index = get_decoding_index(val, shift_value)
+        if str.islower(val):
+            decoded_text = decoded_text + small_letters[char_index]
+        elif str.isupper(val):
+            decoded_text = decoded_text + capital_letters[char_index]
+        elif str.isdigit(val):
+            decoded_text = decoded_text + numbers[char_index]
+        else:
+            decoded_text = decoded_text + val
+
+    return decoded_text
+
+
+def get_decoding_index(c, shift_value):
+    if str.islower(c):
+        for i, val in enumerate(small_letters):
+            if c == val:
+                shift_value = shift_value % len(small_letters)
+                if (i-shift_value) < 0:
+                    return (i - shift_value + len(small_letters)) % len(small_letters)
+                else:
+                    return (i - shift_value) % len(small_letters)
+
+    elif str.isupper(c):
+        for i, val in enumerate(capital_letters):
+            if c == val:
+                shift_value = shift_value % len(capital_letters)
+                if (i-shift_value) < 0:
+                    return (i - shift_value + len(capital_letters)) % len(capital_letters)
+                else:
+                    return (i - shift_value) % len(capital_letters)
+
+    elif str.isdigit(c):
+        for i, val in enumerate(numbers):
+            if c == val:
+                shift_value = shift_value % len(numbers)
+                if (i-shift_value) < 0:
+                    return (i - shift_value + len(numbers)) % len(numbers)
+                else:
+                    return (i - shift_value) % len(numbers)
+
+    else:
+        return -1
---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Author @journey6 (Hussain)
+Github link: https://github.com/journey6/
+this is implementation of Caesar cipher which shifts characters and letters by specific number positive or negative
+"""
+import cipher
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+C = '\033[36m'  # cyan
+W = '\033[0m'   # white
+
+
+# encrypting text
+print(C + "### encrypting plain text ###")
+plain_text = "hi hello welcome back"
+number = -1 # shift value
+cipher_text = cipher.encoder(plain_text, number)
+print(W + "shift number:", number)
+print(W + "plain text:  ", R + plain_text)
+print(W + "cipher text: ", G + cipher_text)
+
+print('')
+
+# decoding text
+print(C + "### decoding cipher text ###")
+cipher_text = "Xlmw mw e wigvix qiwweki"
+number = 4 # shift value
+plain_text = cipher.decoder(cipher_text, number)
+print(W + "shift number: ", number)
+print(W + "cipher text: ", R + cipher_text)
+print(W + "plain text: ", G + plain_text)
+
+
+
+
